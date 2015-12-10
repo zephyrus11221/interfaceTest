6f4dce4f088b98eb74eb876bf3564d81d341c6f9
Stephan Doan <sdoan@stuy.edu>
2015-12-10 13:08:32 -0500

    Add Prism.java

---
+++ Prism.java
@@ -0,0 +1,17 @@
+public class Prism extends Rectangle{
+
+    private double height;
+
+    public Prism(double length, double width, double height, String name){
+	super(length,width,name);
+	this.height = height;
+    }
+
+    public double getHeight(){
+	return height;
+    }
+
+    public String toString(){
+	return "FIXME!";
+    }
+}
