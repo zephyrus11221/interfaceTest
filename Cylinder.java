1f57896b4066039b3e07a29a883489dede12bd09
Stephan Doan <sdoan@stuy.edu>
2015-12-10 13:04:53 -0500

    Add Cylinder.java

---
+++ Cylinder.java
@@ -0,0 +1,17 @@
+public class Cylinder extends Circle{
+
+    private double height;
+
+    public Cylinder(double radius, double height,String name){
+	super(radius,name);
+	this.height = height;
+    }
+
+    public double getHeight(){
+	return height;
+    }
+
+    public String toString(){
+	return "FIXME!";
+    }
+}
