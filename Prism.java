8cde6c801cb8a957f00b5abe01b9f62da10b8bea
Stephan Doan <sdoan@stuy.edu>
2015-12-10 13:10:51 -0500

    Fix Prism.java toString()

--- Prism.java
+++ Prism.java
@@ -12,6 +12,6 @@ public class Prism extends Rectangle{
     }
 
     public String toString(){
-	return "FIXME!";
+	return "Prism "+getName()+" that is "+getLength()+" by "+getWidth()+" by "+height;
     }
 }
