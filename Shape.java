9814294d035f13573a9b12b923dcbff8cf9d2aec
Stephan Doan <sdoan@stuy.edu>
2015-12-10 12:56:30 -0500

    Add Shape.java

---
+++ Shape.java
@@ -0,0 +1,18 @@
+public abstract class Shape{
+
+    private String name;
+
+    public Shape(){
+	name = "";
+    }
+
+    public Shape(String name){
+	this.name = name;
+    }
+
+    public String getName(){
+	return name;
+    }
+
+    public abstract String toString();
+}
