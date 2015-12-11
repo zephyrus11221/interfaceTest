f69ed8e38b519ee50aa7471e16ec9655dc1b5305
Stephan Doan <sdoan@stuy.edu>
2015-12-10 23:37:47 -0500

    Implement Volume in Cylinder.java

--- Cylinder.java
+++ Cylinder.java
@@ -1,4 +1,4 @@
-public class Cylinder extends Circle{
+public class Cylinder extends Circle implements Volume{
 
     private double height;
 
@@ -13,5 +13,8 @@ public class Cylinder extends Circle{
 
     public String toString(){
 	return "Cylinder "+getName()+" with a radius of "+getRadius()+" and a height of "+getHeight();
+    }
+    public double getVolume(){
+	return Math.PI*height*getRadius()*getRadius;
     }
 }
